838054dc84b1c5330ba2a401ac49987e649ed94c
sazuk <20db059@okiu.ac.jp>
2024-01-09 01:59:48 +0900

    デプロイ３

--- app/Models/Announce.php
+++ app/Models/Announce.php
@@ -9,9 +9,5 @@ class Announce extends Model
 {
     use HasFactory;
     
-    public function getPaginateByLimit(int $limit_count = 5)
-{
-    // updated_atで降順に並べたあと、limitで件数制限をかける
-    return $this->orderBy('updated_at', 'DESC')->paginate($limit_count);
-}
+   
 }
